dcc733ecdbe1a70801d79b57bbf9175decc69ca9
mona9855 <mona.magdy2012912012911@gmail.com>
2024-03-02 16:47:49 +0200

    added loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Canvas } from "@react-three/fiber";
-import {Experience} from "./components/Experience";
-import  Interface  from "./components/Interface";
+import { Experience } from "./components/Experience";
+import Interface from "./components/Interface";
+import { Suspense } from "react";
+import Loader from "./components/Loader";
 
 function App() {
   return (
     <>
       <Canvas shadows camera={{ position: [4, 4, -12], fov: 35 }}>
-        <Experience />
+        <Suspense fallback={<Loader />}>
+          <Experience />
+        </Suspense>
       </Canvas>
+
       <Interface />
     </>
   );
